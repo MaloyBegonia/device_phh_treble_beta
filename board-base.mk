da885e5ce0390540f8e3032aaf70683bb9ef3d4e
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2024-07-16 03:44:30 +0300

    upload

---
+++ board-base.mk
@@ -0,0 +1,16 @@
+TARGET_SYSTEM_PROP := device/phh/treble/system.prop $(TARGET_SYSTEM_PROP)
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/phh/treble/bluetooth/include
+
+TARGET_EXFAT_DRIVER := exfat
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/phh/treble/framework_manifest.xml
+
+BOARD_ROOT_EXTRA_FOLDERS += bt_firmware sec_storage efs persist
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+BUILD_BROKEN_DUP_RULES := true
+#BOARD_EXT4_SHARE_DUP_BLOCKS := true
+TARGET_NO_KERNEL_OVERRIDE := true
+
+BOARD_ROOT_EXTRA_SYMLINKS := $(filter-out $(BOARD_ROOT_EXTRA_SYMLINKS),/mnt/vendor/persist:/persist)
